48394f202367c99183e85610b8199bff16ae559a
jahooba <josh.uapena20002@gmail.com>
2024-05-03 14:26:47 -0700

    *Split the url into separate strings to allow customization by user *Added for loops to search for the selected radio button **If the radio button is checked, then the appropriate strings are concatenated in one filterURL to be fetched *Only implemented the first options so far

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
-const url = 'https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=WOurXSGPYOGvLJy2DjGirkxRrWDZnYgP';
+const url = 'https://api.nytimes.com/svc/mostpopular/v2/';
+var sortOption = '';
+var timeOption = '';
+const apiKey = '.json?api-key=WOurXSGPYOGvLJy2DjGirkxRrWDZnYgP';
 
 async function getResponse() {
+    var mostFilter = document.getElementsByName('sort-type');
+    for (let i = 0; i < mostFilter.length; i++) {
+        if (i == 0 && mostFilter[i].checked) {
+            sortOption = 'viewed'; 
+            console.log("Most option = " + sortOption);
+        }
+    }
+
+    var timeFilter = document.getElementsByName('time-type');
+    for (let j = 0; j < timeFilter.length; j++) {
+        if (j == 0 && timeFilter[j].checked){
+            timeOption = '/1';
+            console.log("Time option = " + timeOption);
+        }    
+    }
+    console.log(url);
+    console.log(sortOption);
+    console.log(timeOption);
+    console.log(apiKey);
+    var filterURL = url + sortOption + timeOption + apiKey;
+    console.log(filterURL);
+
     try {
-        const response = await fetch(url);
+        const response = await fetch(filterURL);
 
         if (!response.ok){
             throw new Error("Could not fetch resource");
@@ -11,20 +36,22 @@ async function getResponse() {
         const data = await response.json();
         console.log(data);
 
-        var filter = document.getElementsByName('sort-type');
+        const articleTitle = data.results[0].title;
+        const firstCardTitle = document.querySelector("#first-card-title");
+        firstCardTitle.innerHTML = "1) " + articleTitle;
+        
+        const articleDate = new Date(data.results[0].published_date).toISOString().slice(0, 10);
+        const firstCardDate = document.querySelector("#first-card-date");
+        firstCardDate.innerHTML = articleDate;
 
-        for (let i = 0; i < filter.length; ++i) {
-            if (filter[i].checked) {
-                const articleTitle = data.results[0].title;
-                const articleDate = new Date(data.results[0].published_date).toISOString().slice(0, 10);
+        const articleImageURL = data.results[0].media[0]["media-metadata"][0].url;
+        const firstCardImage = document.querySelector("#first-card-image");
+        firstCardImage.innerHTML = "<img src=" + articleImageURL + ">"
 
-                const firstCardTitle = document.querySelector("#first-card-title");
-                firstCardTitle.innerHTML = "1) " + articleTitle;
-
-                const firstCardDate = document.querySelector("#first-card-date");
-                firstCardDate.innerHTML = articleDate;
-            }
-        }
+        const articleAbstract = data.results[0].abstract;
+        const firstCardAbstract = document.querySelector("#first-card-text");
+        firstCardAbstract.innerHTML = articleAbstract;
+    
     } 
     
     catch (error) {
